f328bbbe31651863794885ac83e1b028005b24d3
Marius <marius.bartcus@gmail.com>
2018-12-13 20:14:18 +0100

    model logit function

--- RHLP/R/utils.R
+++ RHLP/R/utils.R
@@ -209,3 +209,96 @@ IRLS_MixFRHLP <- function(tauijk, phiW, Wg_init=NULL, cluster_weights=NULL, verb
 
   return(list(W, piik, reg_irls, LL, loglik))
 }
+
+
+
+
+modele_logit <- function(Wg, phiW, Y=NULL, Gamma=NULL){
+  #W - Wg
+  #M - phiW
+  #Y - ?
+  #Gamma - cluster_weights
+  if (!is.null(Y)) {
+    n1 <- nrow(Y)
+    K <- ncol(Y)
+    if (!is.null(Gamma)) {
+      Gamma <- Gamma %*% ones(1,K)
+    }
+    n2 <- nrow(phiW)
+    q <- ncol(phiW)
+    if (n1==n2){
+      n <- n1
+    }
+    else{
+      stop("Wg and Y must have the same number of lines")
+    }
+  }
+  else{
+    n <- nrow(phiW)
+    q <- ncol(phiW)
+  }
+
+  if (!is.null(Y)){
+    if (ncol(Wg) == (K-1)){ # pas de vecteur nul dans W donc l'ajouter
+      wK <- zeros(q,1)
+      Wg <- cbind(Wg, wK)
+    }
+    else{
+      stop("Wg and Y must have the same number of lines")
+    }
+  }
+  else{
+    wK <- zeros(q,1)
+    Wg <- cbind(Wg, wK)
+    q <- nrow(Wg)
+    K <- ncol(Wg)
+  }
+
+  MW <- phiW %*% Wg
+  maxm <- apply(MW,1,max)
+  MW <- MW - maxm %*% ones(1,K)
+
+  expMW <- exp(MW)
+  if (ncol(expMW)==1){
+    probas <- expMW / (expMW[,1:K] %*% ones(1,K))
+  }
+  else{
+    probas <- expMW / (rowSums(expMW[,1:K]) %*% ones(1,K))
+  }
+
+
+  if (!is.null(Y)){
+    if (is.null(Gamma)) {
+      loglik <- sum((Y*MW) - (Y*log(rowSums(expMW)%*%ones(1,K))))
+    }
+    else {
+      loglik <- sum(((Gamma*Y)*MW) - ((Gamma*Y)*log(rowSums(expMW)%*%ones(1,K))))
+    }
+
+    if (is.nan(loglik)){
+      MW <- phiW %*% Wg
+      minm <- -745.1
+      MW <- pmax(MW, minm)
+      maxm <- 709.78
+      MW <- pmin(MW, maxm)
+      expMW <- exp(MW)
+
+      eps <- .Machine$double.eps
+
+      if (is.null(Gamma)) {
+        loglik <- sum((Y*MW) - (Y*log(rowSums(expMW)%*%ones(1,K)+eps)))
+      }
+      else {
+        loglik <- sum(((Gamma*Y)*MW) - ((Gamma*Y)*log(rowSums(expMW)%*%ones(1,K)+eps)))
+      }
+    }
+    if (is.nan(loglik)){
+      stop("Probleme loglik NaN (!!!)")
+    }
+  }
+  else{
+    loglik <- c()
+  }
+
+  return(list(probas, loglik))
+}
