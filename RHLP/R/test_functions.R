978e20b714fbfe815f61dd480e7516dd7732ef68
Marius <marius.bartcus@gmail.com>
2018-12-13 17:56:37 +0100

    test initialization of parameter function

--- RHLP/R/test_functions.R
+++ RHLP/R/test_functions.R
@@ -53,3 +53,32 @@ testPhi <- function(){
   return(phi)
 }
 phi <- testPhi()
+
+
+testParameterInitialization <- function(){
+  library(R.matlab)
+  fileName = "data/simulated_time_series.mat"
+  data <- MyData$new()
+  data$setDataFromMat(fileName)
+
+  K <- 3; #nombre de regimes
+  p <- 1; #dimension de beta (ordre de reg polynomiale)
+  q <- 1; #dimension de w (ordre de reg logistique)
+  mix <- MixModel(data,K,p,q)
+
+  n_tries=1
+  max_iter=1000
+  threshold <- 1e-5
+  verbose <- TRUE
+  verbose_IRLS <- TRUE
+  mixOptions <- ModelOptions(n_tries, max_iter, threshold, verbose, verbose_IRLS, variance_types$homoskedastic)
+
+  phi <- Phi$new()
+  phi$setPhi1(mix$x,mix$p,mix$q)
+
+  param <- MixParam(mix, mixOptions)
+  param$initParam(mix, phi, mixOptions, try_algo = 1)
+
+  return(param)
+}
+param <- testParameterInitialization()
