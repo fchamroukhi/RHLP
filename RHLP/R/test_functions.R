945e6870617c45a5a17f412304aead9ceb4f25f7
Marius <marius.bartcus@gmail.com>
2018-12-13 13:59:04 +0100

    ModelOptions and enums of variance type

--- RHLP/R/test_functions.R
+++ RHLP/R/test_functions.R
@@ -9,3 +9,10 @@ testLoadDataFromMat <- function(){
   print(data$m)
 }
 testLoadDataFromMat()
+
+n_tries=1
+max_iter=1000
+threshold <- 1e-5
+verbose <- TRUE
+verbose_IRLS <- TRUE
+mixOptions <- ModelOptions(n_tries, max_iter, threshold, verbose, verbose_IRLS, variance_types$common)
