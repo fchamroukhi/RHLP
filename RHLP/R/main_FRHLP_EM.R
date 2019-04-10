e21c09673fb31ac4fa7e246e08509410876d72f7
Florian Lecocq <florian.lecocq@outlook.com>
2019-04-10 11:48:28 +0200

    Add plots

--- RHLP/R/main_FRHLP_EM.R
+++ RHLP/R/main_FRHLP_EM.R
@@ -28,3 +28,34 @@ solution <- EM(mixModel, modelOptions)
 #mixParamSolution <- solution[[1]]
 #mixStatsSolution <- solution[[2]]
 
+par(mfrow=c(2, 1))
+plot(mixData$y, type = "l", ylab = "y", xlab = "")
+title(main = "Time series, RHLP regimes and process probabilities")
+colors = rainbow(K)
+for (k in 1:K) {
+  index <- (solution[[2]]$klas == k)
+  polynomials <- solution[[2]]$polynomials[(solution[[2]]$klas == k), k]
+  lines(solution[[2]]$polynomials[, k], lty = "dotted", lwd = 2, col = colors[k])
+  lines(seq(1:mixData$m)[index], polynomials, lwd = 2, col = colors[k])
+}
+
+plot(solution[[2]]$h_ig[, 1], type = "l", lwd = 2, col = colors[1], xlab = "x", ylab = expression('Probability ' ~ pi [k] (t, w)))
+for (k in 2:K) {
+  lines(solution[[2]]$h_ig[, k], type = "l", lwd = 2, col = colors[k])
+}
+
+# plot
+par(mfrow=c(2, 1))
+plot(mixData$y, type = "l", ylab = "y", xlab = "", col = "grey")
+title(main = "Time series, estimated RHLP model, and segmentation")
+
+tk = which(diff(solution[[2]]$klas) != 0)
+for (i in 1:length(tk)) {
+  abline(v = tk[i], lty = "dotted", lwd = 2, col = "black")
+}
+for (k in 1:K) {
+  index <- (solution[[2]]$klas == k)
+  polynomials <- solution[[2]]$polynomials[(solution[[2]]$klas == k), k]
+  lines(seq(1:mixData$m)[index], polynomials, lwd = 2, col = "red")
+}
+plot(solution[[2]]$klas, type = "l", lwd = 2, col = "black", xlab = "", ylab = "Estimated class labels")
