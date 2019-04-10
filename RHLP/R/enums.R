5ef41fe746219834387b19cf69a5ba474c5fcf95
Marius <marius.bartcus@gmail.com>
2019-04-10 17:50:27 +0200

    clear folder

--- RHLP/R/enums.R
+++
@@ -1,8 +0,0 @@
-enum <- function(...) {
-  nms <- eval(substitute(alist(...)))
-  x <- as.list(setNames(seq_along(nms), nms))
-  x
-}
-
-variance_types <- enum(homoskedastic, hetereskedastic)
-#algorithms <- enum(em, cem)
