71874ca0944a82d27bfccfec6516280793981ea1
Florian Lecocq <florian.lecocq@outlook.com>
2019-07-11 18:05:39 +0200

    Update documentation

--- R/StatRHLP.R
+++ R/StatRHLP.R
@@ -1,8 +1,11 @@
 #' A Reference Class which contains statistics of a RHLP model.
 #'
 #' StatRHLP contains all the statistics associated to a [RHLP][ParamRHLP] model.
-#' It mainly includes the E-Step of the EM algorithm calculating the posterior distribution of the hidden variables, as well as the calculation of the log-likelhood at each step of the algorithm and the obtained values of model selection criteria..
-#' 
+#' It mainly includes the E-Step of the EM algorithm calculating the posterior
+#' distribution of the hidden variables, as well as the calculation of the
+#' log-likelhood at each step of the algorithm and the obtained values of model
+#' selection criteria..
+#'
 #' @field pi_ik Matrix of size \eqn{(m, K)} representing the prior/logistic
 #'   probabilities \eqn{\pi_{k}(x_{i}; \boldsymbol{\Psi}) = P(z_{i} = k |
 #'   \boldsymbol{x}; \Psi)}{\pi_{k}(x_{i}; \Psi) = P(z_{i} = k | x; \Psi)} of
@@ -61,9 +64,9 @@ StatRHLP <- setRefClass(
     polynomials = "matrix"
   ),
   methods = list(
-    
+
     initialize = function(paramRHLP = ParamRHLP()) {
-      
+
       pi_ik <<- matrix(NA, paramRHLP$m, paramRHLP$K)
       z_ik <<- matrix(NA, paramRHLP$m, paramRHLP$K)
       klas <<- matrix(NA, paramRHLP$m, 1)
@@ -79,17 +82,17 @@ StatRHLP <- setRefClass(
       log_sum_piik_fik <<- matrix(NA, paramRHLP$m, 1)
       tau_ik <<- matrix(0, paramRHLP$m, paramRHLP$K)
       polynomials <<- matrix(NA, paramRHLP$m, paramRHLP$K)
-      
+
     },
-    
+
     MAP = function() {
       "MAP calculates values of the fields \\code{z_ik} and \\code{klas}
       by applying the Maximum A Posteriori Bayes allocation rule.
-      
-      \\eqn{z_{ik} = 1 \\ \textrm{if} \\ k = \\textrm{arg} \\ \\textrm{max}_{s}
+
+      \\eqn{z_{ik} = 1 \\ \\textrm{if} \\ k = \\textrm{arg} \\ \\textrm{max}_{s}
       \\ \\pi_{s}(x_{i}; \\boldsymbol{\\Psi});\\ 0 \\ \\textrm{otherwise}}{
       z_{ik} = 1 if z_ik = arg max_{s} \\pi_{k}(x_{i}; \\Psi); 0 otherwise}"
-      
+
       N <- nrow(pi_ik)
       K <- ncol(pi_ik)
       ikmax <- max.col(pi_ik)
@@ -100,42 +103,42 @@ StatRHLP <- setRefClass(
         klas[z_ik[, k] == 1] <<- k
       }
     },
-    
+
     computeLikelihood = function(reg_irls) {
       "Method to compute the log-likelihood. \\code{reg_irls} is the value of
       the regularization part in the IRLS algorithm."
-      
+
       loglik <<- sum(log_sum_piik_fik) + reg_irls
-      
+
     },
-    
+
     computeStats = function(paramRHLP) {
       "Method used in the EM algorithm to compute statistics based on
       parameters provided by the object \\code{paramRHLP} of class
       \\link{ParamRHLP}."
-      
+
       polynomials <<- paramRHLP$phi$XBeta %*% paramRHLP$beta
       Ex <<- matrix(rowSums(pi_ik * polynomials))
-      
+
       BIC <<- loglik - (paramRHLP$nu * log(paramRHLP$m) / 2)
       AIC <<- loglik - paramRHLP$nu
-      
+
       zik_log_alphag_fg_xij <- (z_ik) * (log_piik_fik)
       com_loglik <<- sum(rowSums(zik_log_alphag_fg_xij))
       ICL <<- com_loglik - paramRHLP$nu * log(paramRHLP$m) / 2
-      
+
     },
-    
+
     EStep = function(paramRHLP) {
       "Method used in the EM algorithm to update statistics based on parameters
       provided by the object \\code{paramRHLP} of class \\link{ParamRHLP}
       (prior and posterior probabilities)."
-      
+
       pi_ik <<- multinomialLogit(paramRHLP$W, paramRHLP$phi$Xw, ones(paramRHLP$m, paramRHLP$K), ones(paramRHLP$m, 1))$piik
-      
+
       for (k in (1:paramRHLP$K)) {
         muk <- paramRHLP$phi$XBeta %*% paramRHLP$beta[, k]
-        
+
         if (paramRHLP$variance_type == "homoskedastic") {
           sigmak <-  paramRHLP$sigma2[1]
         } else {
@@ -145,7 +148,7 @@ StatRHLP <- setRefClass(
         log_piik_fik[, k] <<-
           log(pi_ik[, k]) - (0.5 * ones(paramRHLP$m, 1) * (log(2 * pi) + log(sigmak))) - (0.5 * z)
       }
-      
+
       log_piik_fik <<- pmax(log_piik_fik, log(.Machine$double.xmin))
       piik_fik <- exp(log_piik_fik)
       fxi <- rowSums(piik_fik)
